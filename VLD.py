8c68b8b7edda7b12c414568889e98146eb6662eb
Luanlpg <36865707+Luanlpg@users.noreply.github.com>
2018-06-19 12:04:32 -0300

    Update VLD.py

--- VLD.py
+++ VLD.py
@@ -11,10 +11,9 @@ class VURL():
 
     def valid(self):
 
-        content = requests.get(self.url)
-        print (self.url,'|', content.status_code)
-        if content.status_code != 200:
-            slack_webhook_url = 'https://hooks.slack.com/services/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'
-            payload = {"text": "URL [{}] --- ERROR [{}]".format(self.url,content.status_code)}
-            response = requests.post(slack_webhook_url, json=payload)
+        content = requests.get(self.url) 
+        if content.status_code != 200:#caso o status code seja um valor diferente de 200 o canal recebera um alerta
+            slack_webhook_url = 'https://hooks.slack.com/services/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'#Endereço do canal
+            payload = {"text": "URL [{}] --- ERROR [{}]".format(self.url,content.status_code)}#Text com link quebrado
+            response = requests.post(slack_webhook_url, json=payload)#Post da mensagem no canal do Slack
         return '-------------------------------------------'
